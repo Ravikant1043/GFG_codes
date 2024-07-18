206512bf364f8bef6825a85dcab90c3a6cf08815
Ravikant Sharma <97437176+Ravikant1043@users.noreply.github.com>
2024-07-18 21:02:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest alternating subsequence/longest-alternating-subsequence.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int alternatingMaxLength(vector<int>& arr) {
+        // Code here
+        int a,b;
+        a=b=1;
+        for(int i=1;i<arr.size();i++){
+            if(arr[i]<arr[i-1])b=a+1;
+            if(arr[i]>arr[i-1])a=b+1;
+        }
+        return a>b?a:b;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    cin.ignore();
+    while (tc--) {
+        string s;
+        getline(cin, s);
+        stringstream ss(s);
+        vector<int> arr;
+        string temp;
+        while (ss >> temp) {
+            arr.push_back(stoi(temp));
+        }
+        Solution obj;
+        int ans = obj.alternatingMaxLength(arr);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
