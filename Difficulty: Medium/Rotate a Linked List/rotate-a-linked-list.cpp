0dab8402fffc7fd4897b9903a64cb971f3db8b56
Ravikant Sharma <97437176+Ravikant1043@users.noreply.github.com>
2024-08-10 10:14:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Rotate a Linked List/rotate-a-linked-list.cpp
@@ -0,0 +1,112 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node* next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+void printList(Node* node) {
+    while (node != NULL) {
+        cout << node->data << " ";
+        node = node->next;
+    }
+    cout << "\n";
+}
+
+
+// } Driver Code Ends
+/*
+
+struct Node {
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+
+class Solution
+{
+    public:
+    //Function to rotate a linked list.
+    Node* rotate(Node* head, int k)
+    {
+        vector<int>help;
+        Node *t=head;
+        while(t){
+            help.push_back(t->data);
+            t=t->next;
+        }
+        int n=help.size();
+        k%=n;
+        k=n-k;
+        reverse(help.begin(),help.end());
+        reverse(help.begin(),help.begin()+k);
+        reverse(help.begin()+k,help.end());
+        t=head;
+        int i=0;
+        while(t && i<n){
+            t->data=help[i++];
+            t=t->next;
+        }
+        return head;
+    }
+};
+    
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+
+        // Read numbers from the input line
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Node* head = nullptr;
+
+        // Check if the array is empty
+        if (!arr.empty()) {
+            head = new Node(arr[0]);
+            Node* tail = head;
+            for (size_t i = 1; i < arr.size(); ++i) {
+                tail->next = new Node(arr[i]);
+                tail = tail->next;
+            }
+        }
+        int k;
+        cin >> k;
+        cin.ignore();
+
+        Solution ob;
+        head = ob.rotate(head, k);
+        printList(head);
+    }
+    return 1;
+}
+
+// } Driver Code Ends
